3204e6a2f8eaad5fe15d5e812b57debdb8f03138
CodyAndGity <mdavis@student.neumont.edu>
2025-10-07 10:44:18 -0600

    Added a small blue triangle and a tiny green one

--- Source/Application/Main.cpp
+++ Source/Application/Main.cpp
@@ -33,7 +33,23 @@ int main(int argc, char* argv[]) {
         neu::GetEngine().GetRenderer().Clear();
 
         glBegin(GL_TRIANGLES);
+        glColor3f(0, 1,0);
+        glVertex3f(-0.25f, -0.25f, 0);
+        glColor3f(0, 1,0);
+        glVertex3f(0.25f, -0.25f, 0);
+        glColor3f(0, 1,0);
+        glVertex3f(0, 0.25f, 0);
+        glBegin(GL_TRIANGLES);
 
+        glBegin(GL_TRIANGLES);
+        glColor3f(0, 0, 1);
+        glVertex3f(-0.5f, 0.5f, 0);
+        glColor3f(0, 0, 1);
+        glVertex3f(0.5f, 0.5f, 0);
+        glColor3f(0, 0, 1);
+        glVertex3f(0, -0.5f, 0);
+
+        glBegin(GL_TRIANGLES);
         glColor3f(1,0,0);
         glVertex3f(0,1,0);
         glColor3f(1,0,0);
